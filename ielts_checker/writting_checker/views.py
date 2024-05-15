7d486a54b0d49893f5d6045f2fa11362220367ca
khoavpt <khoahd686@gmail.com>
2024-05-16 02:07:45 +0700

    improve UI

--- ielts_checker/writting_checker/views.py
+++ ielts_checker/writting_checker/views.py
@@ -16,42 +16,53 @@ def home_page(request):
             # Store task and writting in the session
             request.session['task'] = task
             request.session['writting'] = writting
+            request.session['new'] = True
 
             return redirect('writting-result')
         
     return render(request, 'home_page.html', {'form': form})
 
 def result(request):
-
     # Get task and writting from the session
     task = request.session.get('task')
     writting = request.session.get('writting')
-    print(task, writting)
     result = get_writting_result(task, writting)
 
-    # Save to database
-    user_writting = UserWrittings(
-        user_name=request.user,
-        task=task,
-        writting=writting,
-        score=result['score'],
-        feed_back=result['feed_back'],
-        public_status=False 
-    )
+    if request.session.get('new'):
+        # Save to database
+        user_writting = UserWrittings(
+            user_name=request.user,
+            task=task,
+            writting=writting,
+            score=result['score'],
+            feed_back=result['feed_back'],
+            public_status=False 
+        )
 
-    user_writting.save()
+        user_writting.save()
     context = {'result': result, 'task': task, 'writting': writting}
     return render(request, 'writting_result.html', context)
 
 def public_writting(request):
-    return render(request, 'public_writting.html')
+    public_writtings = UserWrittings.objects.filter(public_status=True)
+    return render(request, 'public_writting.html', {'public_writtings': public_writtings})
 
 @login_required(login_url='user-login')
 def writting_history(request):
-    current_user = request.user
+    if request.method == 'POST':
+        task = request.POST.get('task')
+        writting = request.POST.get('writting')
+        # Store task and writting in the session
+        request.session['task'] = task
+        request.session['writting'] = writting
+        request.session['new'] = False
 
-    user_data = UserWrittings.objects.filter(user_name=current_user)
-    print(user_data)
-    return render(request, 'writting_history.html', {'user_data': user_data})
+        return redirect('writting-result')
+    else:
+        current_user = request.user
+
+        user_data = UserWrittings.objects.filter(user_name=current_user)
+        print(user_data)
+        return render(request, 'writting_history.html', {'user_data': user_data})
 
 
